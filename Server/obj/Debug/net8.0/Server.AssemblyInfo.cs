2c94a056448e8bf427387db55b6ae05b8cc999a0
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-01 10:10:12 +0600

    remove obj folder

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e44f33032ae00692cc758897100679d79d3832")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07a8563ae6f8db6d9d4232c2064f71849c30a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
