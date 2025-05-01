27edf904dc2f50b8484654bc02bf0916a6c5bddf
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-01 10:49:41 +0600

    Add Sale

---
+++ Server/Models/Sale.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Models;
+
+public class Sale
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int QuantitySold { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime SaleDate { get; set; } = DateTime.Now;
+    public Product? Product { get; set; }
+}
