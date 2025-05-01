2ed48fcfcba31dd78d95edbbdad9c18d13dc32b2
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-01 10:42:48 +0600

    Add Product

---
+++ Server/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Server.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? SKU { get; set; }
+    public decimal Price { get; set; }
+    public int StockQty { get; set; }
+    public string? Description { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
--- Server/Models/User.cs
+++ Server/Models/User.cs
@@ -4,7 +4,7 @@ public class User
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public string? UserName { get; set; }
-    public string? Password { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
