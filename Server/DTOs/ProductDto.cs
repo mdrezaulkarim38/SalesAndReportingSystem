6acee5d7308c7b8642f186585b51a220ce8d7d35
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-01 16:38:27 +0600

    Create three dtos

---
+++ Server/DTOs/ProductDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+namespace Server.DTOs;
+public class ProductDto
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string? Name { get; set; }
+    [StringLength(50)]
+    public string? SKU { get; set; }
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public decimal Price { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int StockQty { get; set; }
+    [StringLength(500)]
+    public string? Description { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
