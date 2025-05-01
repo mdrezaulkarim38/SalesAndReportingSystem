be63fc161db73c0323d3db589f22ced11c9acb94
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-01 10:31:00 +0600

    add some package and add DbContext

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
