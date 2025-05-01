c07a8563ae6f8db6d9d4232c2064f71849c30a2d
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-01 09:55:48 +0600

    update project for gitignore set up

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f9880792d507f684ec82de15e11e9e1146f4cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e44f33032ae00692cc758897100679d79d3832")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
