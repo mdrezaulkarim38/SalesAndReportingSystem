d58bf93e9810ec0dba2a698cd5dbbe9ec4ca2b5d
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-02 15:57:44 +0600

    login and register api is done

---
+++ client/src/services/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:5174/api";
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {"Content-Type": "application/json"}
+});
+
+export const register = async (name, userName, password) => {
+    return await api.post("/Auth/register", {name, userName, password});
+};
+
+export const login = async (userName, password) => {
+    return await api.post("/Auth/login", {userName, password});
+};
+
+export const logout = () => {
+    localStorage.removeItem("token");
+};
+
+export const getToken = () => {
+    localStorage.getItem("token");
+};
+
+export default api;
